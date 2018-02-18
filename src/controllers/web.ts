8f4d6fe2e949deb51cd3a6ebfa386cd6954554ce
Nikhil Warke <nikwarke@gmail.com>
2018-02-18 11:22:29 +0530

    Send updated device bac
    After db has updated item, send it back to caller

--- src/controllers/web.ts
+++ src/controllers/web.ts
@@ -26,7 +26,6 @@ const getPayload = (data: Device): IotPayload => {
 
 export let iot = (req: Request, res: Response) => {
   const payload: IotPayload = getPayload(req.body);
-  console.log(`Iot`);
 
   const iotDevice = new IotDevice();
 
@@ -34,14 +33,12 @@ export let iot = (req: Request, res: Response) => {
   const isOn = payload.action === 'on';
   DeviceService.deviceService
     .patch(payload.device, { isOn: isOn })
-    .then(item => console.log(`DB Update: ${JSON.stringify(item)}`));
-
-  iotDevice
-    .send(payload)
-    .then(response => {
-      res.send(JSON.stringify({ speech: response, displayText: response }));
-    })
-    .catch(reason => {
-      res.send(JSON.stringify({ speech: reason, displayText: reason }));
+    .then(item => {
+      console.log(`DB Update: ${JSON.stringify(item)}`);
+      res.json(item);
     });
+
+  iotDevice.send(payload).catch(reason => {
+    console.error(`Sending to IOT failed\n${reason}`);
+  });
 };
