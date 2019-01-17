e46261ebf2d856c24cf8f15a23272b59c8875e0e
Nikhil Warke <nwarke@protonmail.com>
2019-01-17 12:09:55 +0530

    Update Group and User models
    Use ids to cross reference both models. It can also be used to check
    access over group and room
    
    Some importand points
      - Hypen (-) separator is used for concatenation
      - Group id is concatenation of user id and group name
      - Room id is concatenation of group id and room name
    
    These ids allows for logical and efficient indexing as well key access
    to values

--- src/iot-device/device-group.ts
+++
@@ -1,6 +0,0 @@
-import { IIotDevice } from './iot-device-model';
-
-export interface IDeviceGroup {
-  devices: IIotDevice[];
-  group: string;
-}
--- src/user/user-model.ts
+++ src/user/user-model.ts
@@ -1,8 +1,9 @@
 import { IClient } from '../client-device/client-model';
 
 export interface IUser {
+  _id: string;
   username: string;
   password: string;
-  groups: string[];
+  groupIds: string[]; // IDeviceGroup._id
   clients: IClient[];
 }
